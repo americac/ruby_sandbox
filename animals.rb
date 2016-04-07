1ea2187fc6386f86dc1c648943a31a5d1ff680c0
America Calleros <america.quintero@gmail.com>
2016-04-07 12:37:04 -0500

    Make name an instance variable

--- animals.rb
+++ animals.rb
@@ -1,41 +1,56 @@
 # Bird class
 class Bird
-  def talk(name)
-    puts "#{name} says Tweet! Tweet!"
+  def make_up_name
+    @name = 'Tweety'
   end
 
-  def move(name, destination)
-    puts "#{name} is flying to the #{destination}."
+  def talk
+    puts "#{@name} says Tweet! Tweet!"
+  end
+
+  def move(destination)
+    puts "#{@name} is flying to the #{destination}."
   end
 end
 
 # Dog class
 class Dog
-  def talk(name)
-    puts "#{name} says Bow Wow!!"
+  def make_up_name
+    @name = 'Tiny'
   end
 
-  def move(name, destination)
-    puts "#{name} and running to the #{destination}."
+  def talk
+    puts "#{@name} says Bow Wow!!"
+  end
+
+  def move(destination)
+    puts "#{@name} and running to the #{destination}."
   end
 end
 
 # Cat class
 class Cat
-  def talk(name)
-    puts "#{name} says Meeeoooow!!"
+  def make_up_name
+    @name = 'Garfield'
   end
 
-  def move(name, destination)
-    puts "#{name} is running to the #{destination}."
+  def talk
+    puts "#{@name} says Meeeoooow!!"
+  end
+
+  def move(destination)
+    puts "#{@name} is running to the #{destination}."
   end
 end
 
 bird = Bird.new
-bird.move('Tweety', 'tree')
+bird.make_up_name
+bird.move('tree')
 
 dog = Dog.new
-dog.move('Tiny', 'house')
+dog.make_up_name
+dog.move('house')
 
 cat = Cat.new
-cat.move('Garfield', 'window')
+cat.make_up_name
+cat.move('window')
