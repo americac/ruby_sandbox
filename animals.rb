2ef7752d6d7a1bb7e3e14ac7f3e52883d986260d
America Calleros <america.quintero@gmail.com>
2016-04-18 13:12:27 -0500

    Create Animal superclass and have other classes inherit

--- animals.rb
+++ animals.rb
@@ -1,9 +1,47 @@
-# Bird class
-class Bird
-  def make_up_name
-    @name = 'Tweety'
+# Animal class
+class Animal
+  attr_reader :name, :age, :type
+
+  def name(value)
+    if value == ''
+      raise "Name can't be blank!"
+    end
+    @name = value
   end
 
+  def age(value)
+    if value < 0
+      raise "An age of #{value} isn't valid!!"
+    end
+    @age = value
+  end
+
+  def type(value)
+    if !value
+      raise "Type cannot be blank!!"
+    end
+    @type = value
+  end
+
+  def report_age
+    if !@age
+      raise "I Don't know how old #{@name} is."
+    end
+    puts "#{@name} is #{@age} years old."
+  end
+
+  def report_type
+    if !@type
+      raise "I Don't know what #{@name} is."
+    eslif !@name
+      raise "I need a name!!!"
+    end
+    puts "#{@name} is #{@type}."
+  end
+end
+
+# Bird class
+class Bird < Animal
   def talk
     puts "#{@name} says Tweet! Tweet!"
   end
@@ -14,25 +52,19 @@ class Bird
 end
 
 # Dog class
-class Dog
-  attr_accessor :name, :age
-
+class Dog < Animal
   def talk
     puts "#{@name} says Bow Wow!!"
   end
 
   def move(destination)
-    puts "#{@name} and running to the #{destination}."
+    puts "#{@name} is running to the #{destination}."
   end
 
 end
 
 # Cat class
-class Cat
-  def make_up_name
-    @name = 'Garfield'
-  end
-
+class Cat < Animal
   def talk
     puts "#{@name} says Meeeoooow!!"
   end
@@ -40,30 +72,32 @@ class Cat
   def move(destination)
     puts "#{@name} is running to the #{destination}."
   end
-
-  def make_up_age
-    @age = 5
-  end
-
-  def report_age
-    puts "#{@name} is #{@age} years old."
-  end
 end
 
 bird = Bird.new
-bird.make_up_name
+bird.name('Tweety')
 bird.move('tree')
+bird.type('Bird')
 
 dog = Dog.new
-dog.name='Tiny'
-dog.age=3
+dog.name('Tiny')
+dog.age(3)
 dog.move('house')
-age = dog.age
-name = dog.name
-puts "#{name} is #{age} years old"
+dog.report_age
+dog.type('Dog')
 
 cat = Cat.new
-cat.make_up_name
+cat.name('Bandit')
 cat.move('window')
-cat.make_up_age
+cat.age(2)
 cat.report_age
+cat.type('Cat')
+
+dog2 = Dog.new
+dog2.name('Oso')
+dog2.talk
+dog2.age(10)
+dog2.report_age
+dog2.move('Tree')
+dog2.type('Dog')
+dog2.report_type
