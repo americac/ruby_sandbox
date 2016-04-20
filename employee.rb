88bb5ffed15f34d2f8720c08bfa64b171e03d134
America Calleros <america.quintero@gmail.com>
2016-04-19 19:00:45 -0500

    Leave Employee super class and create salaried employee class

--- employee.rb
+++ employee.rb
@@ -1,10 +1,31 @@
 # Employee class
 class Employee
-  attr_reader :name, :salary
+  attr_reader :name
 
   def name(name)
     raise "Name can't be blank!" if name == ''
     @name = name
+  end
+
+  def print_name
+    puts "Employee name: #{@name}"
+  end
+
+ # def initialize(name = 'Anonymouse', salary = 0.0)
+ #   self.name(name)
+ #   self.salary(salary)
+ # end
+
+end
+
+class SalariedEmployee < Employee
+  attr_reader :salary
+
+  def print_pay_stub
+    print_name
+    pay_for_period = (@salary / 365.0) * 14
+    pay_for_period = format('$%.2f', pay_for_period)
+    puts "Pay This Period: #{pay_for_period}"
   end
 
   def salary(salary)
@@ -12,15 +33,4 @@ class Employee
     @salary = salary
   end
 
-  def initialize(name = 'Anonymouse', salary = 0.0)
-    self.name(name)
-    self.salary(salary)
-  end
-
-  def print_pay_stub
-    puts "Name: #{@name}"
-    pay_for_period = (@salary / 365.0) * 14
-    pay_for_period = format('$%.2f', pay_for_period)
-    puts "Pay This Period: #{pay_for_period}"
-  end
 end
