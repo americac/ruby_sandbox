50eacc6afe8fdeb375c2dc123d50e887d675e47f
America Calleros <america.quintero@gmail.com>
2016-04-20 13:07:45 -0500

    Use each method to loop through arrays

--- arrays.rb
+++ arrays.rb
@@ -1,38 +1,26 @@
 def calculate
   amount = 0
-  index = 0
-  while index < prices.length
-    yield prices
-  end
+  prices.each do |price| yield end
   amount
 end
 
 def total(prices)
   amount = 0
-  index = 0
-  while index < prices.length
-    amount += prices[index]
-    index += 1
-  end
+  prices.each do |price| amount += price end
   amount
 end
 
 def refund(prices)
   amount = 0
-  index = 0
-  while index < prices.length
-    amount -= prices[index]
-    index += 1
-  end
+  prices.each do |price| amount -= price end
   amount
 end
 
 def show_discounts(prices)
-  index = 0
-  while index < prices.length
-    amount_off = prices[index] / 3.0
-    puts format('%.2f', amount_off)
-    index += 1
+  amount_off = nil
+  prices.each do |price|
+    amount_off = price / 3.0
+    puts format("Your discount is: %.2f", amount_off)
   end
 end
 prices = [3.00, 25.00, 8.99]
@@ -40,7 +28,7 @@ puts format('%.2f', total(prices))
 puts format('%.2f', refund(prices))
 show_discounts(prices)
 
-calculate do|prices|
-  amount -= prices[index]
-end
-puts format('%.2f', total_amount)
+#calculate do|prices|
+#  amount -= prices[index]
+#end
+#puts format('%.2f', total_amount)
