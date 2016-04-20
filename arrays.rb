8c0a5067a0753532396e1b3d546fbc398b04d46f
America Calleros <america.quintero@gmail.com>
2016-04-19 20:00:25 -0500

    Add method to display discounts

--- arrays.rb
+++ arrays.rb
@@ -17,6 +17,16 @@ def refund(prices)
   end
   amount
 end
+
+def show_discounts(prices)
+  index = 0
+  while index < prices.length
+    amount_off = prices[index] / 3.0
+    puts format('%.2f', amount_off)
+    index += 1
+  end
+end
 prices = [3.00, 25.00, 8.99]
 puts format('%.2f', total(prices))
 puts format('%.2f', refund(prices))
+show_discounts(prices)
