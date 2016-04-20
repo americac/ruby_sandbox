666fb810a18d044efe370f2aad8539053c794fc1
America Calleros <america.quintero@gmail.com>
2016-04-19 19:43:51 -0500

    Create HourlyEmployee class and initialize methods

--- employee.rb
+++ employee.rb
@@ -11,13 +11,11 @@ class Employee
     puts "Employee name: #{@name}"
   end
 
- # def initialize(name = 'Anonymouse', salary = 0.0)
- #   self.name(name)
- #   self.salary(salary)
- # end
-
+  def initialize(name = 'Anonymous')
+    self.name(name)
+  end
 end
-
+# SalariedEmployee class
 class SalariedEmployee < Employee
   attr_reader :salary
 
@@ -33,4 +31,47 @@ class SalariedEmployee < Employee
     @salary = salary
   end
 
+  def initialize(name = "Anonymous",salary = 0.0)
+    super(name)
+    self.salary(salary)
+  end
+end
+# Hourly Employee class
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage(hourly_wage)
+    raise "A wage of #{hourly_wage} isn't valid!!" if hourly_wage < 0
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week(hours_per_week)
+    raise "#{hours_per_week} hours is not valid!" if hours_per_week < 0
+    @hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = @hourly_wage * @hours_per_week * 2
+    pay_for_period = format('$%.2f', pay_for_period)
+    puts "Pay This Period: #{pay_for_period}"
+  end
+
+  def initialize(name = "Anonymous",hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage(hourly_wage)
+    self.hours_per_week(hours_per_week)
+  end
+
+  def self.security_guard(name)
+    HourlyEmployee.new(name, 19.25, 30)
+  end
+
+  def self.cashier(name)
+    HourlyEmployee.new(name, 12.75, 25)
+  end
+
+  def self.janitor(name)
+    HourlyEmployee.new(name, 10.50, 20)
+  end
 end
