a503a36c26399e24d785896fe9d4d1963bb8ddd8
America Calleros <america.quintero@gmail.com>
2016-04-20 14:11:23 -0500

    Add open file from within a block

--- file_parse.rb
+++ file_parse.rb
@@ -1,6 +1,23 @@
-review_file = File.open('reviews.txt')
-puts review_file.class
+#review_file = File.open('reviews.txt')
+#puts review_file.class
 
-lines = review_file.readlines
-puts "Line 4: #{lines[3]}"
-puts "Line 1: #{lines[0]}"
+#lines = review_file.readlines
+#puts "Line 4: #{lines[3]}"
+#puts "Line 1: #{lines[0]}"
+#review_file.close #close the file safely
+
+#Open a file from within a block, automatically closes file for you!
+lines = nil
+File.open('reviews.txt') do |review_file|
+  lines = review_file.readlines
+end
+puts "Line 3: #{lines[2]}"
+puts "Line 5: #{lines[6]}"
+
+relevant_lines = [] 
+lines.each do |line|
+  if line.include?("Truncated")
+    relevant_lines << line
+  end
+end
+puts relevant_lines
