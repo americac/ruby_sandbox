f620232fcad0a45633015634a677fa7b371ed231
America Calleros <america.quintero@gmail.com>
2016-04-19 19:58:07 -0500

    Create method to generate refunds

--- arrays.rb
+++ arrays.rb
@@ -8,6 +8,15 @@ def total(prices)
   amount
 end
 
+def refund(prices)
+  amount = 0
+  index = 0
+  while index < prices.length
+    amount -= prices[index]
+    index += 1
+  end
+  amount
+end
 prices = [3.00, 25.00, 8.99]
-total_cost = total(prices)
-puts format('%.2f', total_cost)
+puts format('%.2f', total(prices))
+puts format('%.2f', refund(prices))
