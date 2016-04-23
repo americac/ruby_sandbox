cd5cbd570840c7934b78d3f19c386bfba6196302
America Calleros <america.quintero@gmail.com>
2016-04-23 07:27:00 -0500

    Parse out adjectives from input file

--- file_parse.rb
+++ file_parse.rb
@@ -7,7 +7,7 @@
 #review_file.close #close the file safely
 
 #Open a file from within a block, automatically closes file for you!
-lines = nil
+lines = []
 File.open('reviews.txt') do |review_file|
   lines = review_file.readlines
 end
@@ -24,3 +24,19 @@ puts "Line 5: #{lines[6]}"
 #This is the same as above, much cleaner and faster
 relevant_lines = lines.find_all { |line| line.include?('Truncated') }
 puts relevant_lines
+
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+lines = []
+File.open("reviews.txt") do |review_file|
+  lines = review_file.readlines
+end
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+reviews = relevant_lines.reject { |line| line.include?("--") }
+adjectives = reviews.map do |review|
+  adjective = find_adjective(review) "'#{adjective.capitalize}'"
+end
+puts "The critics agree, Truncated is:" puts adjectives
