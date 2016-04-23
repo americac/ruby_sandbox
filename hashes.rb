cb33f6f5aed631e70c5dfff9bc62d7761122dde3
America Calleros <america.quintero@gmail.com>
2016-04-23 06:48:29 -0500

    Check for hash key and values

--- hashes.rb
+++ hashes.rb
@@ -2,3 +2,7 @@ protons = {"H" => 1, "Li" => 3, "Ne" => 10}
 puts protons.class
 puts protons.inspect
 puts protons.length
+puts protons.has_key?('Ne')
+puts protons.has_key?('X')
+puts protons.has_value?(3)
+puts protons.has_value?(5)
