c0b25fe28311346432f12f50eaff8a6ae7d1c423
America Calleros <america.quintero@gmail.com>
2016-04-26 20:26:27 -0500

    Create hash to compare values with

--- meat.rb
+++ meat.rb
@@ -1,7 +1,16 @@
+# Steak Class
 class Steak
   attr_accessor :grade
+
+  def >(other)
+    grade_scores = { 'Prime' => 3, 'Choice' => 2, 'Select' => 1 }
+    grade_scores[grade] > grade_scores[other.grade]
+  end
 end
 
-if first_steak > second_steak
-  puts "I'll take #{first_steak}"
-end
+first_steak = Steak.new
+first_steak.grade = 'Prime'
+second_steak = Steak.new
+second_steak.grade = 'Choice'
+
+puts "I'll take #{first_steak.inspect}" if first_steak > second_steak
