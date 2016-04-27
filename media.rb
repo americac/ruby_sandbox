6da7805f6b04a8398a813859251a8aafd0096c37
America Calleros <america.quintero@gmail.com>
2016-04-26 20:01:36 -0500

    Move module to top, it matters

--- media.rb
+++ media.rb
@@ -1,19 +1,4 @@
-class Clip
-  attr_reader :comments
-
-  def play
-    puts "Playing #{object_id}..."
-  end
-end
-
-class Video < Clip
-  attr_accessor :resolution
-end
-
-class Song < Clip
-  attr_accessor :beats_per_minute
-end
-
+# Module for comments
 module AcceptsComments
   def comments
     if @comments
@@ -27,11 +12,29 @@ module AcceptsComments
     comments << comment
   end
 end
+# Clip class
+class Clip
+  attr_reader :comments
+
+  def play
+    puts "Playing #{object_id}..."
+  end
+end
+# Video class
+class Video < Clip
+  include AcceptsComments
+  attr_accessor :resolution
+end
+# Song class
+class Song < Clip
+  include AcceptsComments
+  attr_accessor :beats_per_minute
+end
 
 video = Video.new
-video.add_comment("Cool slow motion effect!")
-video.add_comment("Weird ending.")
+video.add_comment('Cool slow motion effect!')
+video.add_comment('Weird ending.')
 song = Song.new
-song.add_comment("Awesome beat.")
+song.add_comment('Awesome beat.')
 
 p video.comments, song.comments
