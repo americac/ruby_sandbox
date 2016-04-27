2438f83aee628a6d07ef5696d5d8c6a22dbe4836
America Calleros <america.quintero@gmail.com>
2016-04-26 20:43:06 -0500

    Use Comparable mixin

--- meat.rb
+++ meat.rb
@@ -1,5 +1,6 @@
 # Steak Class
 class Steak
+  include Comparable
   attr_accessor :grade
   GRADE_SCORES = { 'Prime' => 3, 'Choice' => 2, 'Select' => 1 }
 
@@ -8,7 +9,7 @@ class Steak
   end
 
   def <=>(other)
-    if GRADE_SCORES[self.grade] <GRADE_SCORES[other.grade]
+    if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
       return -1
     elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
       return 0
@@ -25,3 +26,18 @@ second_steak.grade = 'Choice'
 
 puts "I'll take #{first_steak.inspect}" if first_steak > second_steak
 puts second_steak <=> first_steak
+
+prime = Steak.new
+prime.grade = 'Prime'
+choice = Steak.new
+choice.grade = 'Choice'
+select = Steak.new
+select.grade = 'Select'
+
+puts "prime > choice: #{prime > choice}"
+puts "prime < select: #{prime < select}"
+puts "select == select: #{select == select}"
+puts "select <= select: #{select <= select}"
+puts "select >= choice: #{select >= choice}"
+puts "choice.between?(select, prime): "
+puts choice.between?(select, prime)
